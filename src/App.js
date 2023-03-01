d10e756da611d03f4fa414daf79184401a230d8f
Turing <shspku365@gmail.com>
2023-03-01 07:16:41 +0000

    add orbitcontrols

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import * as THREE from "three";
 import { useEffect } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(100, 100, 100);
 const material = new THREE.MeshLambertMaterial({
-  color: 0x0000ff
+  color: 0x0000ff,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -36,7 +37,17 @@ const render = () => {
   const dom = document.getElementById("node");
   dom.appendChild(renderer.domElement);
   renderer.render(scene, camera);
+  // 每次绕y轴旋转0.01弧度
+  mesh.rotateY(0.01);
+  requestAnimationFrame(render);
 };
+// 定义了一个鼠标、键盘事件，自动检测鼠标键盘的变化，如果变化了就会自动更新相机的数据
+const controls = new OrbitControls(camera, renderer.domElement);
+// 如果threejs代码中通过requestAnimationFrame()实现渲染器渲染方法render()的周期性调用，
+// 当通过OrbitControls操作改变相机状态的时候，
+// 没必要在通过controls.addEventListener('change', render)监听鼠标事件调用渲染函数，
+// 因为requestAnimationFrame()就会不停的调用渲染函数。
+// controls.addEventListener("change", render);
 
 export default function App() {
   useEffect(() => {
